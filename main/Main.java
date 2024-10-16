22d1024dcc1ed9a4e6d5a05b51784670cab77f92
luca-chierchia <chierchialuca92@gmail.com>
2024-10-16 22:31:46 +0200

    modifiche Main e classe Utente.java

--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,23 @@
 package main;
+import java.time.LocalDate;
+import java.time.*;
+import model.*;
+
+
 
 public class Main {
+	public static void main(String[] args) {
+		Utente u2 = new Utente("Gianpiercarlo", "De Pascalis");
+		LocalDate d =  LocalDate.now();
+		Libro l1 = new Libro("Stephan King","983-234421-46760","It");
+		Libro l2 = new Libro("Stephan King","983-234421-46760","It");
+		Libro l3 = new Libro("Alessandro Manzoni", "144-3423400-230", "I promessi Sposi");
+		Prestito p = new Prestito(new Utente("Massimo", "Troisi"),l1, d);
+		
+		System.out.println(p);
+		
+	
+	}
+	
 
 }
--- model/Utente.java
+++ model/Utente.java
@@ -1,5 +1,61 @@
 package model;
 
 public class Utente {
+	private String nome,cognome;
+	private static int idCounter = 1000;
+	private int id;
+	
+	public Utente(String n, String c) {
+		this.nome = n;
+		this.cognome = c;
+		this.id = this.idCounter++;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 17;
+		return result = 31 * 17 + this.id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null || this.getClass() != obj.getClass())
+			return false;
+		Utente user = (Utente) obj;
+		return this.getNome().equals(user.getNome()) && this.getCognome().equals(user.getCognome());
+	}
+
+
+	@Override
+	public String toString() {
+		return "Utente: " + this.getNome() + " " + this.getCognome() + "\nID: " + this.getId();
+	}
+	
 
 }
