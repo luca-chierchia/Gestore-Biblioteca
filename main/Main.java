324e05539648d2b8567455c0ae49289cb4422492
luca-chierchia <chierchialuca92@gmail.com>
2024-10-18 21:55:24 +0200

    Aggiunto GestoreCatalogo + modifiche generali

--- main/Main.java
+++ main/Main.java
@@ -1,20 +1,24 @@
 package main;
 import java.time.LocalDate;
-import java.time.*;
 import model.*;
+import service.*;
 
 
 
 public class Main {
 	public static void main(String[] args) {
+		String str = "I promessi Sposi";
 		Utente u2 = new Utente("Gianpiercarlo", "De Pascalis");
 		LocalDate d =  LocalDate.now();
 		Libro l1 = new Libro("Stephan King","983-234421-46760","It");
 		Libro l2 = new Libro("Stephan King","983-234421-46760","It");
 		Libro l3 = new Libro("Alessandro Manzoni", "144-3423400-230", "I promessi Sposi");
 		Prestito p = new Prestito(new Utente("Massimo", "Troisi"),l1, d);
-		
-		System.out.println(p);
+		GestoreCatalogo catalogo = new GestoreCatalogo();
+		catalogo.aggiungiLibro(l3);
+		System.out.println(catalogo.getDimensioneCatalogo());
+		Libro libroCercato = catalogo.cercaLibroPerTitolo(str);
+		System.out.println(libroCercato.getAutore());
 		
 	
 	}
--- service/GestoreCatalogo.java
+++ service/GestoreCatalogo.java
@@ -1,5 +1,61 @@
 package service;
+import model.*;
+import java.util.*;
+import errorGestoreBiblioteca.*;
 
+/*	La classe GestoreCatalogo ci permette di aggiungere,cercare e rimuovere libri al
+	catalogo.
+*/
 public class GestoreCatalogo {
-
+	private List<Libro> catalogo;
+	private int dimensioneCatalogo = 0;
+	
+	public GestoreCatalogo() {
+		this.catalogo = new ArrayList<Libro>();
+	}
+	
+	public int getDimensioneCatalogo() {
+		return this.dimensioneCatalogo;
+	}
+	
+	public void aggiungiLibro(Libro l) {
+		this.catalogo.add(l);
+		this.dimensioneCatalogo++;
+		System.out.println(l.getTitolo() + ", aggiunto.");
+	}
+	
+	public Libro cercaLibroPerIsbn(String s) {
+		Iterator<Libro> it = this.catalogo.iterator();
+		while(it.hasNext()) {
+			Libro libro = it.next();
+			if(libro.getIsbn().equals(s))
+				return libro;
+		}
+		return null;
+				
+	}
+	
+	public Libro cercaLibroPerTitolo(String t) {
+		Iterator<Libro> it = this.catalogo.iterator();
+		while(it.hasNext()) {
+			Libro libro = it.next();
+			if(libro.getTitolo().equals(t))
+				return libro;
+		}
+		return null;
+		
+	}
+	
+	public boolean rimuoviLibro(String s) {
+		Iterator<Libro> it = this.catalogo.iterator();
+		while(it.hasNext()) {
+			Libro libro = it.next();
+			if(libro.getIsbn().equals(s)) {
+				it.remove();
+				this.dimensioneCatalogo--;
+				return true;
+			}
+		}
+		return false;
+	}
 }
